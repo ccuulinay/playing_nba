7b88b5fd49017b3633b4fe6b0213e6e78aae26cb
ccuulinay <ccuulinay@gmail.com>
2017-03-13 14:17:13 +0800

    Add spider getting tomorrows asian handicap and odds.

---
+++ nba_odds_ah_pregetter/nba_odds_ah_pregetter/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.item import Item, Field
+
+
+class NbaOddsAhPregetterItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    date_time = Field()
+    home_team = Field()
+    away_team = Field()
+    ah_1 = Field()
+    odd_cnt_1 = Field()
+    odd_home_1 = Field()
+    odd_away_1 = Field()
+
